c98df3034060fa59e0fe092c49a7b24590757701
Craig Houston <craighouston01@gmail.com>
2018-01-28 16:34:17 +0000

    Added followers and updated unit tests

--- app/models.py
+++ app/models.py
@@ -1,6 +1,11 @@
 from app import db
 from hashlib import md5
 
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     firstname = db.Column(db.String(64))
@@ -10,6 +15,12 @@ class User(db.Model):
     reviews = db.relationship('Review', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime)
+    followed = db.relationship('User',
+                               secondary=followers,
+                               primaryjoin=(followers.c.follower_id == id),
+                               secondaryjoin=(followers.c.followed_id == id),
+                               backref=db.backref('followers', lazy='dynamic'),
+                               lazy='dynamic')
 
     def avatar(self, size):
         return 'http://www.gravatar.com/avatar/%s?d=mm&s=%d' % (md5(self.email.encode('utf-8')).hexdigest(), size)
@@ -43,6 +54,23 @@ class User(db.Model):
                 break
             version += 1
         return new_nickname
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+            return self
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+            return self
+
+    def is_following(self, user):
+        return self.followed.filter(followers.c.followed_id == user.id
+            ).count() > 0
+
+    def followed_reviews(self):
+        return Review.query.join(followers, (followers.c.followed_id == Review.user_id)).filter(followers.c.follower_id == self.id).order_by(Review.timestamp.desc())
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
--- tests.py
+++ tests.py
@@ -1,10 +1,11 @@
 #!flask/bin/python
 import os
 import unittest
+from datetime import datetime, timedelta
 
 from config import basedir
 from app import app, db
-from app.models import User
+from app.models import User, Review
 
 class TestCase(unittest.TestCase):
     def setUp(self):
@@ -36,6 +37,79 @@ class TestCase(unittest.TestCase):
         nickname2 = User.make_unique_nickname('john')
         assert nickname2 != 'john'
         assert nickname2 != nickname
+
+    def test_follow(self):
+        u1 = User(nickname='john', email='john@example.com')
+        u2 = User(nickname='susan', email='susan@example.com')
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.commit()
+        assert u1.unfollow(u2) is None
+        u = u1.follow(u2)
+        db.session.add(u)
+        db.session.commit()
+        assert u1.follow(u2) is None
+        assert u1.is_following(u2)
+        assert u1.followed.count() == 1
+        assert u1.followed.first().nickname == 'susan'
+        assert u2.followers.count() == 1
+        assert u2.followers.first().nickname == 'john'
+        u = u1.unfollow(u2)
+        assert u is not None
+        db.session.add(u)
+        db.session.commit()
+        assert not u1.is_following(u2)
+        assert u1.followed.count() == 0
+        assert u2.followers.count() == 0
+
+    def test_follow_posts(self):
+        # make four users
+        u1 = User(nickname='john', email='john@example.com')
+        u2 = User(nickname='susan', email='susan@example.com')
+        u3 = User(nickname='mary', email='mary@example.com')
+        u4 = User(nickname='david', email='david@example.com')
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.add(u3)
+        db.session.add(u4)
+        # make four reviews
+        utcnow = datetime.utcnow()
+        r1 = Review(body="Review from john", author=u1, timestamp=utcnow + timedelta(seconds=1))
+        r2 = Review(body="Review from susan", author=u2, timestamp=utcnow + timedelta(seconds=2))
+        r3 = Review(body="Review from mary", author=u3, timestamp=utcnow + timedelta(seconds=3))
+        r4 = Review(body="Review from david", author=u4, timestamp=utcnow + timedelta(seconds=4))
+        db.session.add(r1)
+        db.session.add(r2)
+        db.session.add(r3)
+        db.session.add(r4)
+        db.session.commit()
+        # setup the followers
+        u1.follow(u1)  # john follows himself
+        u1.follow(u2)  # john follows susan
+        u1.follow(u4)  # john follows david
+        u2.follow(u2)  # susan follows herself
+        u2.follow(u3)  # susan follows mary
+        u3.follow(u3)  # mary follows herself
+        u3.follow(u4)  # mary follows david
+        u4.follow(u4)  # david follows himself
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.add(u3)
+        db.session.add(u4)
+        db.session.commit()
+        # check the followed reviews of each user
+        f1 = u1.followed_reviews().all()
+        f2 = u2.followed_reviews().all()
+        f3 = u3.followed_reviews().all()
+        f4 = u4.followed_reviews().all()
+        assert len(f1) == 3
+        assert len(f2) == 2
+        assert len(f3) == 2
+        assert len(f4) == 1
+        assert f1 == [r4, r2, r1]
+        assert f2 == [r3, r2]
+        assert f3 == [r4, r3]
+        assert f4 == [r4]
 
 if __name__ == '__main__':
     unittest.main()
